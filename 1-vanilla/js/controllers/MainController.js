4a437084e84905fad9822822e01d80a8777b917e
s-ong-c <somony9292@gmail.com>
2019-02-19 14:33:51 +0900

    1 검색폼 구현 및 입력시 x 지우기

--- 1-vanilla/js/controllers/MainController.js
+++ 1-vanilla/js/controllers/MainController.js
@@ -1,8 +1,11 @@
+import FormView from '../views/FormView.js'
+
 const tag = '[MainController]'
 
 export default{
     init(){
-        console.log(tag,'init()');
+        console.log(tag,'init()')
+        FormView.setup(document.querySelector('form'))
         
     }
 }
