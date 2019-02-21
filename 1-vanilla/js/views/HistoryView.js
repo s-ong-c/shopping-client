b444bf40727bbc5f924094987aeafbebbe2fd612
s-ong-c <somony9292@gmail.com>
2019-02-21 16:52:45 +0900

    검색

--- 1-vanilla/js/views/HistoryView.js
+++ 1-vanilla/js/views/HistoryView.js
@@ -20,21 +20,22 @@ HistoryView.render = function (data = []) {
     this.show()
 }
 HistoryView.getKeywordsHtml = function (data){
-   return data.reduce((html,item,index)=>{
+   return data.reduce((html,item)=>{
        html +=`<li data-keyword="${item.keyword}">
-       <span class="number">${index +1}</span>
        ${item.keyword}
+       <span class="date">${item.date}</span>
+       <button class="btn-remove"></button>
        </li>`
        return html
    },'<ul class="list">')+'</ul>'
 }
-HistoryView.bindClickEvent = function () {
-    Array.from(this.el.querySelectorAll('li')).forEach(li =>{
-        li.addEventListener('click',e=>this.onClickKeyword(e))
-    })
-}
-HistoryView.onClickKeyword = function(e){
-    const{keyword} = e.currentTarget.dataset
-    this.emit('@click',{keyword})
-}
+// HistoryView.bindClickEvent = function () {
+//     Array.from(this.el.querySelectorAll('li')).forEach(li =>{
+//         li.addEventListener('click',e=>this.onClickKeyword(e))
+//     })
+// }
+// HistoryView.onClickKeyword = function(e){
+//     const{keyword} = e.currentTarget.dataset
+//     this.emit('@click',{keyword})
+// }
 export default HistoryView
