7111ce42be64f7fa433d899750854953cbfb709a
s-ong-c <somony9292@gmail.com>
2019-02-22 12:27:33 +0900

    result component 구현

--- 3-component/js/app.js
+++ 3-component/js/app.js
@@ -3,6 +3,8 @@ import KeywordModel from './models/KeywordModel.js'
 import HistoryModel from './models/HistoryModel.js'
 
 import FormComponent from './components/FormComponent.js'
+import ResultComponent from './components/ResultComponent.js'
+
 new Vue({
   el: '#app',
   data: {
@@ -15,7 +17,8 @@ new Vue({
     searchResult: []
   },
   components:{
-    'search-form': FormComponent
+    'search-form': FormComponent,
+    'search-result':ResultComponent
   },
   created() {
     this.selectedTab = this.tabs[0]
