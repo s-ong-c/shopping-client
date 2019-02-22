99f2b3c6ab9561bcb4e3f77b7ffdae274bac64b4
s-ong-c <somony9292@gmail.com>
2019-02-22 10:29:09 +0900

    v-on:

--- 2-vue/js/app.js
+++ 2-vue/js/app.js
@@ -5,7 +5,15 @@ new Vue({
     },
     methods: {
         onSubmit(e){
-                        
+
+        },
+        onKeyup(){
+            if(!this.query.length) this.onReset()
+        },
+        onReset(){
+            this.query=''
+            //todo 검색 결과를 숨기는 .....
+            debugger
         }
     }
 })
